c4e75cc6c57bfd57f080586f6591ab24cb891157
conrod101 <conrod.smith36@gmail.com>
2023-05-16 11:48:05 -0500

    push 22

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv()  # load environment variables from .env if it exists.
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'willg'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        'postgres://', 'postgresql://') or \
+    """Base Config Object"""
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgres://pqywxozuyggfhi:01d47a7ec504394eb60c4ad90d7bbf4c9e41316f2ebd37081e11d953d55103d5@ec2-54-156-8-21.compute-1.amazonaws.com:5432/d2d26sjgdqln9r
+') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
